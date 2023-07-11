a969f3fb2be64b2bbc324858aed2a2314faa276f
Pedro Santana <134099838+pasantanav@users.noreply.github.com>
2023-07-11 01:42:03 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,17 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  let text = "";
+  for (let a in adj) {
+    for (let p in pronoun) {
+      for (let n in noun) {
+        document.getElementById("domain").innerHTML +=
+          pronoun[p] + adj[a] + noun[n] + ".com" + "<br>";
+      }
+    }
+  }
 };
